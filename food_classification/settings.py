473a4ae2304b811059fe6fbeca7adb3d4bf2cf11
Mohamed <samym5021@gmail.com>
2024-04-23 16:56:27 +0200

    test

--- food_classification/settings.py
+++ food_classification/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'meal_details.apps.MealDetailsConfig',
+    'nutrition_systems.apps.NutritionSystemsConfig',
     'rest_framework',
     'rest_framework.authtoken',
     "crispy_forms",
